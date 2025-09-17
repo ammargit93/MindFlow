8ffcce86fa0dbdeda96db3b2ee443d66bd3c5a4b
Ammar <ammar.ice98@gmail.com>
2025-09-17 22:10:27 +0530

    integration works

--- MindFlow/app/config.py
+++ MindFlow/app/config.py
@@ -11,11 +11,15 @@ default_cfg = loaded['defaults']
 
 routes = []
 
+DEFAULT_CFG_MAPPER = {
+    "chroma": ChromaIndex,
+    "faiss": FAISSIndex
+}
 
 class RouteIndex:
-    def __init__(self, Route, FAISSIndex):
+    def __init__(self, Route, ChromaIndex):
         self.route = Route
-        self.index = FAISSIndex
+        self.index = ChromaIndex
         
         
 for config in route_cfg:
@@ -27,11 +31,13 @@ for config in route_cfg:
     
     route = Route(route_name=collection_name, model_card=llm, inference_provider=inference_provider, api_url=api_url)
     # chroma_index = ChromaIndex(collection_name=collection_name, llm=llm)
+    # faiss_index = FAISSIndex(collection_name=collection_name,llm=llm)
     
-    faiss_index = FAISSIndex(collection_name=collection_name,llm=llm)
-    route_index = RouteIndex(route, faiss_index)
+    general_index = DEFAULT_CFG_MAPPER[default_cfg["vectordb"]](collection_name=collection_name, llm=llm)
     
-    # chroma_index.add_document(documents=document, ids=[str(uuid.uuid1()) for _ in document])
-    faiss_index.add_document(documents=document)
+    route_index = RouteIndex(route, general_index)
+    
+    general_index.add_document(documents=document)
+    # faiss_index.add_document(documents=document)
     routes.append(route_index)
         
