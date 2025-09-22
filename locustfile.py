34f1d51ebc92bde05ae65465db4adbd2a83c63c3
Ammar <ammar.ice98@gmail.com>
2025-09-22 17:48:08 +0530

    latest

--- locustfile.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class MindFlowUser(HttpUser):
-    wait_time = between(1, 5)
-
-    @task
-    def query_router(self):
-        self.client.get("/mindflow/api/", params={"query": "tell me about the president"})
